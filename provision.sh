1af127bc2a362064dc5b2e892c5d34bebef15f7a
crynobone <crynobone@gmail.com>
2013-10-09 14:16:49 +0800

    Update provision script
    Signed-off-by: crynobone <crynobone@gmail.com>

--- provision.sh
+++ provision.sh
@@ -1,5 +1,6 @@
 git pull
 chmod -Rf 777 app/storage
+chown -R www-data:www-data public/packages
+chown -Rf www-data:www-data app/storage
 echo "<?php return \"production\";" > bootstrap/env.php
 composer install --no-dev
-
