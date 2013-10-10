7c128347ad78618ab5f845e1e148b1786fb9f257
crynobone <crynobone@gmail.com>
2013-10-10 16:10:50 +0800

    Swap chown process since composer install is republishing the asset
    Signed-off-by: crynobone <crynobone@gmail.com>

--- provision.sh
+++ provision.sh
@@ -1,7 +1,7 @@
 git pull
 git submodule update
 chmod -Rf 777 app/storage
-chown -R www-data:www-data public/packages
 chown -R www-data:www-data app/storage
 echo "<?php return \"production\";" > bootstrap/env.php
 composer install --no-dev
+chown -R www-data:www-data public/packages
