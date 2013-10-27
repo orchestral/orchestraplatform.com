f4e7264679f4d43eabeb4611d183b9ec159a0f1c
crynobone <crynobone@gmail.com>
2013-10-27 23:18:08 +0800

    Update deps and stop supervisord during deployment.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- provision.sh
+++ provision.sh
@@ -1,3 +1,5 @@
+git artisan down
+supervisorctl stop all
 git pull
 git submodule update
 chmod -Rf 777 app/storage
@@ -5,3 +7,5 @@ chown -R www-data:www-data app/storage
 echo "<?php return \"production\";" > bootstrap/env.php
 composer install --no-dev
 chown -R www-data:www-data public/packages
+supervisorctl start all
+php artisan up
