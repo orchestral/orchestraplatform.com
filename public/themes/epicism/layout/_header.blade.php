174c860761e9e379258da87f4d9ab4d1f3d39ab7
crynobone <crynobone@gmail.com>
2013-12-11 07:52:04 +0800

    Remove responsive option
    Signed-off-by: crynobone <crynobone@gmail.com>

--- public/themes/epicism/layout/_header.blade.php
+++ public/themes/epicism/layout/_header.blade.php
@@ -1,6 +1,5 @@
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=9,chrome=1">
-<meta name="viewport" content="width=device-width,initial-scale = 1.0,maximum-scale=1.0,user-scalable=no" />
 <meta name="author" content="{{ memorize('site.name') }}">
 <meta name="description" content="{{ memorize('site.description') }}">
 
