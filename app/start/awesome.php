0d7a804c715c5bf420537abaf209b20cbf9158fc
crynobone <crynobone@gmail.com>
2013-12-19 07:52:27 +0800

    Fixed invalid code.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- app/start/awesome.php
+++ app/start/awesome.php
@@ -6,7 +6,7 @@ use Monolog\Logger;
 $token = Config::get('monolog.hipchat.token');
 $room  = Config::get('monolog.hipchat.room');
 
-if (! (is_null($token) and is_null($room)) {
+if (! (is_null($token) and is_null($room))) {
     $hipchat = new HipChatHandler($token, $room, 'Monolog', true, Logger::INFO);
 
     Log::getMonolog()->pushHandler($hipchat);
