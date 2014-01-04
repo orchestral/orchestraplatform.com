cf30c64cd94992ba30405dc8e4e1cead063992d5
crynobone <crynobone@gmail.com>
2014-01-05 00:03:30 +0800

    Add alias for Widget.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- app/config/app.php
+++ app/config/app.php
@@ -198,6 +198,7 @@ return array(
         'URL'             => 'Illuminate\Support\Facades\URL',
         'Validator'       => 'Illuminate\Support\Facades\Validator',
         'View'            => 'Illuminate\Support\Facades\View',
+        'Widget'          => 'Orchestra\Support\Facades\Widget',
     ),
 
 );
