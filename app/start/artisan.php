f72c60c767b5c0bdab0a03686eddcb9b431e2be9
crynobone <crynobone@gmail.com>
2014-01-21 23:30:31 +0800

    Fixes Service Provider lists and swap to stable version. Closes #9.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- app/start/artisan.php
+++ app/start/artisan.php
@@ -11,14 +11,3 @@
 |
 */
 
-App::register('Illuminate\Foundation\Providers\CommandCreatorServiceProvider');
-App::register('Illuminate\Foundation\Providers\ComposerServiceProvider');
-App::register('Illuminate\Foundation\Providers\KeyGeneratorServiceProvider');
-App::register('Illuminate\Foundation\Providers\MaintenanceServiceProvider');
-App::register('Illuminate\Foundation\Providers\OptimizeServiceProvider');
-App::register('Illuminate\Foundation\Providers\RouteListServiceProvider');
-App::register('Illuminate\Foundation\Providers\ServerServiceProvider');
-App::register('Illuminate\Foundation\Providers\TinkerServiceProvider');
-
-App::register('Orchestra\Optimize\OptimizeServiceProvider');
-App::register('Orchestra\Debug\CommandServiceProvider');
