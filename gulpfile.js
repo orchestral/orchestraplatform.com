1e4404901d3fdafd79b726b1ea3d4fad5003cd32
crynobone <crynobone@gmail.com>
2014-02-12 20:04:42 +0800

    Use gulpfile. Closes #14.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,46 @@
 var gulp = require('gulp'),
-    gutil = require('gulp-util');
+    gutil = require('gulp-util'),
+    coffee = require('gulp-coffee'),
+    csso = require('gulp-minify-css'),
+    less = require('gulp-less'),
+    uglify = require('gulp-uglify');
 
+dir = 'public/themes/epicism/assets';
 
+gulp.task('less', function () {
+    gulp.src(dir + '/css/style.less')
+        .pipe(less({
+            paths: [
+                dir + '/css'
+            ]
+        }))
+        .pipe(gulp.dest(dir + '/css'));
+});
+
+gulp.task('css', function () {
+    gulp.src(dir + '/css/style.css')
+        .pipe(csso())
+        .pipe(gulp.dest(dir + '/css'));
+});
+
+gulp.task('coffee', function () {
+    var options = {
+        outSourceMaps: false,
+        output: {
+            max_line_len: 150
+        }
+    };
+
+    return gulp.src(dir + '/js/script.coffee')
+        .pipe(coffee().on('error', gutil.log))
+        .pipe(uglify(options))
+        .pipe(gulp.dest(dir + '/js'));
+});
+
+gulp.task('watch', function () {
+    gulp.watch(dir + '/css/*.less', ['less']);
+    gulp.watch(dir + '/css/style.css', ['css']);
+    gulp.watch(dir + '/js/script.coffee', ['coffee']);
+});
+
+gulp.task('default', ['less', 'css', 'coffee', 'watch']);
