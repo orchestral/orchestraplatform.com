18d35d25b503777acc650ad65f386b5978f571bf
crynobone <crynobone@gmail.com>
2014-05-04 16:16:52 +0800

    Add cipher
    Signed-off-by: crynobone <crynobone@gmail.com>

--- app/config/app.php
+++ app/config/app.php
@@ -67,6 +67,8 @@ return array(
 
     'key' => 'H7Unue6FQbbuDufHoJAe6mHuMEw8OJX1',
 
+    'cipher' => MCRYPT_RIJNDAEL_256,
+
     /*
     |--------------------------------------------------------------------------
     | Autoloaded Service Providers
