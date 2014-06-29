4cb1f5b2c03e105caf50f100bdb8c0b6bed0cc29
crynobone <crynobone@gmail.com>
2014-06-29 22:55:27 +0800

    Add basic .env configuration.
    Signed-off-by: crynobone <crynobone@gmail.com>

---
+++ .env.sample.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'DB' => [
+        'HOST'     => '127.0.0.1',
+        'DATABASE' => 'orchestraplatform',
+        'USERNAME' => 'root',
+        'PASSWORD' => 'root',
+    ],
+];
--- app/config/database.php
+++ app/config/database.php
@@ -48,10 +48,10 @@ return array(
 
         'mysql' => array(
             'driver'    => 'mysql',
-            'host'      => getenv('DB_HOST'),
-            'database'  => getenv('DB_DATABASE'),
-            'username'  => getenv('DB_USERNAME'),
-            'password'  => getenv('DB_PASSWORD'),
+            'host'      => getenv('DB.HOST'),
+            'database'  => getenv('DB.DATABASE'),
+            'username'  => getenv('DB.USERNAME'),
+            'password'  => getenv('DB.PASSWORD'),
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
