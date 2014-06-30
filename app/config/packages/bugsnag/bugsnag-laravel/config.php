7b6ca39604bdcc1b5a5a87b10ed87656872f3d95
crynobone <crynobone@gmail.com>
2014-06-30 11:04:13 +0800

    Add bugsnag key.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- app/config/packages/bugsnag/bugsnag-laravel/config.php
+++ app/config/packages/bugsnag/bugsnag-laravel/config.php
@@ -3,5 +3,5 @@
      * Set your Bugsnag API Key.
      * You can find your API Key on your Bugsnag dashboard.
      */
-    'api_key' => 'YOUR-API-KEY-HERE'
+    'api_key' => getenv('BUGSNAG.KEY')
 );
