949bac75a78842ad8b2f4ac257848505909b9524
crynobone <crynobone@gmail.com>
2014-11-23 20:41:04 +0800

    Update to orchestral/platform@760b693
    Signed-off-by: crynobone <crynobone@gmail.com>

--- config/services.php
+++ config/services.php
@@ -28,6 +28,12 @@ return [
         'secret' => '',
     ],
 
+    'ses' => [
+        'key' => '',
+        'secret' => '',
+        'region' => 'us-east-1',
+    ],
+
     'stripe' => [
         'model'  => 'User',
         'secret' => '',
