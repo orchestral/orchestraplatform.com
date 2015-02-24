b7995280e496b62f10a81b50fc765090f0e58b66
Izuddin Helmi <izuddin.helmi@gmail.com>
2015-02-24 19:19:50 +0800

    docs styling, fix border, fix header, fix sidebar

--- resources/views/documentation.blade.php
+++ resources/views/documentation.blade.php
@@ -13,11 +13,15 @@
         <div id="sidebar" class="col-md-3">
             {!! array_get($html, 'toc') !!}
         </div>
-        <div id="content" class="col-md-8 col-md-offset-1">
-            <div class="page-header">
-                <h1>{!! $document->get('title') !!}</h1>
+        <div id="content" class="col-md-9">
+            <div class="col-md-1">
             </div>
-            {!! array_get($html, 'document') !!}
+            <div class="col-md-11">
+                <div class="page-header">
+                    <h1>{!! $document->get('title') !!}</h1>
+                </div>
+                {!! array_get($html, 'document') !!}
+            </div>
         </div>
     </div>
 </section>
