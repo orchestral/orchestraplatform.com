3a02b1c6a2fda01b36dda0041e5deab78209e0e8
crynobone <crynobone@gmail.com>
2015-02-26 09:00:49 +0800

    Add download modal.
    Signed-off-by: crynobone <crynobone@gmail.com>

---
+++ resources/views/layouts/_download.blade.php
@@ -0,0 +1,16 @@
+<div class="modal fade" id="download-modal">
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+				<h4 class="modal-title">Download Orchestra Platform</h4>
+			</div>
+			<div class="modal-body">
+				<p>Download Latest Stable:</p>
+				<pre><code class="language-bash">composer create-project orchestra/platform website 3.0.x</code></pre>
+				<p>Download Bleeding Edge:</p>
+				<pre><code class="language-bash">composer create-project orchestra/platform website dev-master</code></pre>
+			</div>
+		</div><!-- /.modal-content -->
+	</div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -4,7 +4,7 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta name="description" content="">
+	<meta name="description" content="Orchestra Platform">
 	<meta name="author" content="Orchestra Platform Development Team">
 
 	{!! HTML::title() !!}
