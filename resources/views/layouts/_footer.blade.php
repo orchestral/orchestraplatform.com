4ee6d7e515f9df6d70ab3dec0e4e715e3092081b
crynobone <crynobone@gmail.com>
2015-03-05 08:24:38 +0800

    Add design by @izuddinhelmi.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -4,13 +4,18 @@
 				<div class="col-md-6">
 					&copy; 2014 - {{ date('Y') }} Orchestra Platform, hosted with <a href="https://www.digitalocean.com/?refcode=e1f9dab486bc" target="_blank">DigitalOcean</a>.
 				</div>
-				<div class="col-md-6">
+				<div class="col-md-6 hidden-xs">
 					<ul class="list-inline pull-right navigation">
 						<li><a href="{{ handles('orchestra/story::/') }}">Blog</a></li>
 						<li><a href="https://www.facebook.com/groups/orchestraplatform/" target="_blank">Forum</a></li>
 						<li class="docs"><a href="{{ handles('app::docs/3.0') }}">Documentation</a></li>
 						<li class="dload"><a href="#" data-toggle="modal" data-target="#download-modal">Download</a></li>
 					</ul>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-12">
+					Design by <a href="https://dribbble.com/izuddinhelmi" target="_blank">Izuddin Helmi</a>
 				</div>
 			</div>
 		</div>
