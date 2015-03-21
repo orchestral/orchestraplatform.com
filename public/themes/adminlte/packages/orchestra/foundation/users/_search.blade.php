e19667147843a0d91653c16d333c4d09c515369f
crynobone <crynobone@gmail.com>
2015-03-21 12:16:31 +0800

    Update adminlte.
    Signed-off-by: crynobone <crynobone@gmail.com>

---
+++ public/themes/adminlte/packages/orchestra/foundation/users/_search.blade.php
@@ -0,0 +1,7 @@
+<div class="navbar user-search">
+    <form class="navbar-form">
+        {!! app('form')->text('q', app('request')->input('q', ''), ['placeholder' => 'Search keyword...', 'role' => 'keyword', 'class' => 'form-control']) !!}
+        {!! app('form')->select('roles[]', $roles, app('request')->input('roles', []), ['multiple' => true, 'placeholder' => 'Roles', 'role' => 'roles']) !!}
+        {!! app('form')->submit(trans('orchestra/foundation::label.search.button'), ['class' => 'btn btn-primary']) !!}
+    </form>
+</div>
