a0ca55bf6b979ad9b0b85dbf010a0a9c92b47064
crynobone <crynobone@gmail.com>
2015-05-27 20:34:50 +0800

    Update docs.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- build-doc.sh
+++ build-doc.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-VERSIONS=("2.0" "2.1" "2.2" "3.0");
+VERSIONS=("2.0" "2.1" "2.2" "3.0" "3.1");
 
 cd docs;
 
@@ -13,8 +13,9 @@ do
 done
 
 # Update master branch
-cd 3.1;
-git checkout master;
+# cd 3.1;
+# git checkout master;
+# cd ../;
 
-cd ../..;
+cd ../;
 git submodule foreach git pull;
