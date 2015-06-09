09ffccd6acf96940e1b7020c6317b8bdca5162a9
crynobone <crynobone@gmail.com>
2015-06-09 10:11:32 +0800

    Update docs setup.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- resources/config/project.php
+++ resources/config/project.php
@@ -1,12 +1,13 @@
 <?php
 
 return [
+    'latest' => '3.1',
     'documentation' => [
         'aliases' => [
             'dev'    => '3.1',
-            'latest' => '3.0',
-            'stable' => '3.0',
-            'lts'    => '2.1',
+            'latest' => '3.1',
+            'stable' => '3.1',
+            'lts'    => '3.1',
             '2.3'    => '3.0',
         ],
     ],
--- resources/views/welcome/_intro.blade.php
+++ resources/views/welcome/_intro.blade.php
@@ -2,7 +2,7 @@
 	<div class="container">
 		<div class="col-md-8 col-md-offset-2">
 			<h1><span>Orchestra Platform</span> provide all the boilerplate for your application, so you can create awesomeness.</h1>
-			<pre><code>composer create-project orchestra/platform website '3.0.x'</code></pre>
+			<pre><code>composer create-project orchestra/platform website '{{ config('project.latest') }}.x'</code></pre>
 		</div>
 	</div>
 
