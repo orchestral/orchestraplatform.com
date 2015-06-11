3b79e99d478e4c567d08e06c5ca6d35e0811bfef
crynobone <crynobone@gmail.com>
2015-06-11 09:42:13 +0800

    Add github badge.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- resources/views/welcome/_intro.blade.php
+++ resources/views/welcome/_intro.blade.php
@@ -3,8 +3,14 @@
 		<div class="col-md-8 col-md-offset-2">
 			<h1><span>Orchestra Platform</span> provide all the boilerplate for your application, so you can create awesomeness.</h1>
 			<pre><code>composer create-project orchestra/platform website '{{ config('project.latest') }}.x'</code></pre>
+
+			<iframe src="https://ghbtns.com/github-btn.html?user=orchestral&repo=platform&type=star&count=true&size=small" frameborder="0" scrolling="0" width="100px" height="20px"></iframe>
+			<iframe src="https://ghbtns.com/github-btn.html?user=orchestral&repo=platform&type=watch&count=true&size=small&v=2" frameborder="0" scrolling="0" width="110px" height="20px"></iframe>
+			<iframe src="https://ghbtns.com/github-btn.html?user=orchestral&repo=platform&type=fork&count=true&size=small" frameborder="0" scrolling="0" width="100px" height="20px"></iframe>
 		</div>
 	</div>
+
+
 
 	<div class="hidden-xs">
 		<img src="{{ asset('resources/img/chrome@2x.png') }}" width="956">
