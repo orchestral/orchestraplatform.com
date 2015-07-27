06a6a38aab37d893ce8170085bb3b1772d70f9a3
crynobone <crynobone@gmail.com>
2015-07-28 07:27:10 +0800

    Update deps and theme.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- public/themes/adminlte/packages/orchestra/foundation/layouts/_header.blade.php
+++ public/themes/adminlte/packages/orchestra/foundation/layouts/_header.blade.php
@@ -1,5 +1,6 @@
 <meta charset="UTF-8">
 @title()
+<meta name="csrf-token" content="{{ csrf_token() }}">
 <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
 
 <?php $asset = app('orchestra.asset')->container('orchestra.header');
