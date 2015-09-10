4434749932aea91f556799e34113ab0f93591b6f
Steve Bauman <steven_bauman@outlook.com>
2015-09-10 13:18:17 -0400

    Update composer create-project code
    - Tiny change. Copying and pasting the original into a cmd window gives an error saying "Could not parse version constraint '3.1.x': Invalid version string"

--- resources/views/welcome/_intro.blade.php
+++ resources/views/welcome/_intro.blade.php
@@ -2,7 +2,7 @@
 	<div class="container">
 		<div class="col-md-8 col-md-offset-2">
 			<h1><span>Orchestra Platform</span> provide all the boilerplate for your application, so you can create awesomeness.</h1>
-			<pre><code>composer create-project orchestra/platform website '{{ config('project.latest') }}.x'</code></pre>
+			<pre><code>composer create-project orchestra/platform website "{{ config('project.latest') }}.*"</code></pre>
 
 			<iframe src="https://ghbtns.com/github-btn.html?user=orchestral&amp;repo=platform&amp;type=star&amp;count=true&amp;size=small" frameborder="0" scrolling="0" width="100px" height="20px"></iframe>
 			<iframe src="https://ghbtns.com/github-btn.html?user=orchestral&amp;repo=platform&amp;type=watch&amp;count=true&amp;size=small&amp;v=2" frameborder="0" scrolling="0" width="110px" height="20px"></iframe>
