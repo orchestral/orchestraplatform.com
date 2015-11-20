6d08f6756214001e7733b4f8e4d60f7261315536
crynobone <crynobone@gmail.com>
2015-11-20 08:28:35 +0800

    Update deps and prepare 3.2 docs.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- build-doc.sh
+++ build-doc.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-VERSIONS=("2.0" "2.1" "2.2" "3.0" "3.1");
+VERSIONS=("2.0" "2.1" "2.2" "3.0" "3.1" "3.2");
 
 cd docs;
 
@@ -13,7 +13,7 @@ do
 done
 
 # Update master branch
-cd 3.2;
+cd 3.3;
 git checkout master;
 cd ../;
 
