3ee15914ce7a3ad8fef3ddabc96d34b250c2bd44
crynobone <crynobone@gmail.com>
2015-12-21 07:14:43 +0800

    Update deps.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends HttpKernel
      */
     protected $middleware = [
         \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
-        // \Fideloper\Proxy\TrustProxies::class,
+        \Fideloper\Proxy\TrustProxies::class,
     ];
 
     /**
