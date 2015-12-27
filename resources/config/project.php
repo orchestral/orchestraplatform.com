8aad28f5d714b5a942aa4db3109f13d0aad87861
crynobone <crynobone@gmail.com>
2015-12-27 13:14:04 +0800

    Update project version.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- resources/config/project.php
+++ resources/config/project.php
@@ -1,12 +1,12 @@
 <?php
 
 return [
-    'latest'        => '3.1',
+    'latest'        => '3.2',
     'documentation' => [
         'aliases' => [
-            'dev'    => '3.2',
-            'latest' => '3.1',
-            'stable' => '3.1',
+            'dev'    => '3.3',
+            'latest' => '3.2',
+            'stable' => '3.2',
             'lts'    => '3.1',
         ],
     ],
