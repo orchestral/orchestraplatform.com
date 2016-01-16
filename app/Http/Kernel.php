88cdf06fdfac4c83d15ffa99561b09f074c2d98a
crynobone <crynobone@gmail.com>
2016-01-16 08:13:02 +0800

    Add not modified middleware.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -13,6 +13,7 @@ class Kernel extends HttpKernel
      */
     protected $middleware = [
         \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
+        \Orchestra\Http\Middleware\NotModified::class,
         \Fideloper\Proxy\TrustProxies::class,
     ];
 
